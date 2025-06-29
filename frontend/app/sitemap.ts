c4186d7d7f0e9bde57125b73c2380fb81efecef7
kawamoto <krt34kk0@gmail.com>
2025-06-29 16:47:17 +0900

    サイトマップ修正

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -1,9 +1,11 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://guranares.com'
+  
   return [
     {
-      url: 'https://guranares.com',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
